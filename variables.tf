0fd3ae1a89bf5988db7f0fcd49a54ffd7d6cc230
jonasmarquez <jonas@jonasmarquez.com>
2023-10-03 21:57:27 +0200

    delete variables

--- variables.tf
+++ variables.tf
@@ -5,12 +5,6 @@ variable "region" {
   type    = string
   default = "eu-central-1"
 }
-variable "aws_access_key" {
-  type = string
-}
-variable "aws_secret_key" {
-  type = string
-}
 # ------------------------------------------------------------------------------
 # CUSTOM VARS
 # You must provide a custom variables
